da9a753f72af44cabb5f170812854252decc42a4
Gustavo Andrade <technologicallo6@gmail.com>
2026-05-27 07:03:37 -0300

    Adição de comentários no backend

--- backend/mvnw.cmd
+++ backend/mvnw.cmd
@@ -1,8 +1,8 @@
-@echo off
+@echo off // isso serve pra nao imprimir os comando do .cmd antes de rodar
 setlocal
 
 set MAVEN_VERSION=3.9.9
-set MAVEN_HOME=%USERPROFILE%\.m2\apache-maven-%MAVEN_VERSION%
+set MAVEN_HOME=%USERPROFILE%\.m2\apache-maven-%MAVEN_VERSION% // Ao Rodar o projeto, aqui verifica se o Maven esta instalado, caso nn tenha, ele baixa automaticamente
 set MAVEN_CMD=%MAVEN_HOME%\bin\mvn.cmd
 
 if not exist "%MAVEN_CMD%" (
@@ -13,6 +13,6 @@ if not exist "%MAVEN_CMD%" (
     echo [INFO] Maven instalado com sucesso.
 )
 
-"%MAVEN_CMD%" %*
+"%MAVEN_CMD%" %* // %* é um atalho do batch que significa "tudo que o usuário digitou depois do nome do script". Sem ele, o Maven rodaria sem saber o que fazer.
 
 endlocal
